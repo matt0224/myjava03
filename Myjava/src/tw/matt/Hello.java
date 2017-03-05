6f8c5881b4f807f453ea234814a5722210b0517d
Mac <Mac@Mac-PC>
2017-03-05 13:27:51 +0800

    20170503

--- Myjava/src/tw/matt/Hello.java
+++ Myjava/src/tw/matt/Hello.java
@@ -3,7 +3,8 @@ package tw.matt;
 public class Hello {
 	public static void main(String[] matt){
 		System.out.print("Hello, World");
-		System.out.println();
+		System.out.println("11224");
+		
 	}
 
 }
